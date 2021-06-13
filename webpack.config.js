09806e05d1b7805bd100dab1ca51fdd079ce0178
sean <sean.hk127@gmail.com>
2021-06-13 13:14:03 -0700

    Templatize webpack compile targets

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,21 @@
 const path = require('path')
 
-module.exports = [
-  {
-    mode: 'production',
-    entry: './src/gallery/gallery.jsx',
-    output: {
-      // put generated javascripts here to be picked up by jekyll
-      path: path.resolve(__dirname, 'docs/js/gallery'),
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react']
-            }
-          }
-        }
-      ]
-    }
-  },
+const MODULES = [
+  'boombox',
+  'gallery'
+]
 
-  {
+/**
+ * Given a module name, return a compile target for Bundle.
+ * Entry point will be at /src/${bundle name}/entry.js and /docs/js/${bundle name}
+ */
+module.exports = MODULES.map((moduleName) => {
+  return {
     mode: 'production',
-    entry: './src/another/another.jsx',
+    entry: `./src/${moduleName}/entry.js`,
     output: {
       // put generated javascripts here to be picked up by jekyll
-      path: path.resolve(__dirname, 'docs/js/another'),
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react']
-            }
-          }
-        }
-      ]
-    }
-  },
-
-  {
-    mode: 'production',
-    entry: './src/boombox/boombox.js',
-    output: {
-      // put generated javascripts here to be picked up by jekyll
-      path: path.resolve(__dirname, 'docs/js/boombox'),
+      path: path.resolve(__dirname, `docs/js/${moduleName}`),
       filename: 'bundle.js'
     },
     module: {
@@ -72,4 +33,5 @@ module.exports = [
       ]
     }
   }
-];
+});
+
