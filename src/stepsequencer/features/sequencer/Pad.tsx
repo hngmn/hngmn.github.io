bcd8086abf2e18e97baff861f5965c07af04e62b
sean <sean.hk127@gmail.com>
2021-06-28 14:00:42 -0700

    test css for pad

--- src/stepsequencer/features/sequencer/Pad.tsx
+++ src/stepsequencer/features/sequencer/Pad.tsx
@@ -1,9 +1,9 @@
 'use strict';
 
 import * as React from 'react';
-
 import { useSelector } from 'react-redux';
 
+import './Pad.css';
 import { useAppDispatch, RootState } from '../../app/store';
 import {
     // actions
@@ -27,14 +27,12 @@ export default function Pad(props: Props) {
     const isOn = useSelector((state: RootState) => selectPad(state, instrumentName, padi));
     const dispatch = useAppDispatch();
 
-    const className = 'someClassName'; // TODO: this should make it so I can program styling?
+    const className = isOn ? 'padOn' : 'padOff';
 
     return (
         <button
             className={className}
             onClick={() => dispatch(padClick(instrumentName, padi))}
-        >
-            <span>{isOn ? '[X]' : '[  ]'}</span>
-        </button>
+        />
     );
 }
