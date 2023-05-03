65c58777b14089a5c23c75e9a646b08dce1ffa89
sean <sean.hk127@gmail.com>
2023-05-03 13:05:24 -0700

    p5 first commit. got sketch to render in viz module

--- src/viz/entry.js
+++ src/viz/entry.js
@@ -1,8 +1,13 @@
 'use strict';
 
+import p5 from 'p5';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import sketch from './sketch';
+
 
 const domContainer = document.querySelector('#viz_container');
 ReactDOM.render(<App/>, domContainer);
+export const myp5 = new p5(sketch, domContainer);
