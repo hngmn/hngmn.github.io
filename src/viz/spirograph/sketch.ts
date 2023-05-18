1e601b9b842cec20aac5fe11a2394dd248bd7df9
sean <sean.hk127@gmail.com>
2023-05-18 14:56:52 -0700

    input

--- src/viz/spirograph/sketch.ts
+++ src/viz/spirograph/sketch.ts
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import p5 from 'p5';
-import { Linter } from 'eslint';
+import PlayButton from '../../stepsequencer/features/sequencer/PlayButton';
 
 export default function spirograph(p: p5) {
-    let NUMSINES = 16; // how many of these things can we do at once?
     let R = 260; // outer circle radius
     let r = 80;  // inner circle radius
     let rho = 17;  // inner circle point radius
     let t = 0;
     let dt = 0.02;
+    let pvec = p.createVector(R - r + rho, 0);
+
+    let Rinput: p5.Element, rinput: p5.Element, rhoinput: p5.Element, button;
 
     let spirofn = (t: number) => {
         const tp = 0 - (((R - r) / r) * t);
@@ -18,11 +20,37 @@ export default function spirograph(p: p5) {
         ];
     }
 
-    let pvec = p.createVector(R - r + rho, 0);
-
     p.setup = () => {
         p.createCanvas(1000, 700);
         p.background(204); // clear the screen
+
+        let inputY = 100;
+        Rinput = p.createInput(String(R));
+        Rinput.position(20, inputY);
+        inputY += 30
+        rinput = p.createInput(String(r));
+        rinput.position(20, inputY);
+        rinput.value(r);
+        inputY += 30
+        rhoinput = p.createInput();
+        rhoinput.position(20, inputY);
+        rhoinput.value(String(rho));
+        inputY += 30
+        button = p.createButton('Add');
+        button.position(20, inputY);
+        button.mousePressed(update);
+        inputY += 30;
+        let clearbutton = p.createButton('Clear');
+        clearbutton.position(20, inputY);
+        clearbutton.mousePressed(() => p.background(204));
+    }
+
+    function update() {
+        R = Number(Rinput.value());
+        r = Number(rinput.value());
+        rho = Number(rhoinput.value());
+        pvec = p.createVector(R - r + rho, 0);
+        t = 0;
     }
 
     p.draw = () => {
